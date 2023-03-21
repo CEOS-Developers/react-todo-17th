fc416fc47d88e0360dfc17029a834524676cc456
yeji <oyatpeach@naver.com>
2023-03-21 09:40:45 +0900

    feat: create Form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import React, { useState } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import Form from './components/Form';
+import Todo from './components/Todo';
+import Done from './components/Done';
+
 function App() {
+  const [list, setList] = useState([]);
+
+  const getTodo = (value) => {
+    const newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    };
+    setList((prev) => [...prev, newTodo]);
+  };
+
   return (
-    <div>
-      <h1>17기 프론트 화이팅~ 우하하</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <Conatiner>
+        <Form getTodo={getTodo}></Form>
+        <Todo></Todo>
+        <Done></Done>
+      </Conatiner>
+    </>
   );
 }
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: rgb(186, 191, 225);
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  `;
+const Conatiner = styled.div`
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  height: 650px;
+  width: 350px;
+  border-radius: 20px;
+  box-shadow: 1px 1px 15px rgba(73, 71, 71, 0.5);
+`;
 
 export default App;
