df6b999ea8af26741ea69e200d0185e306b4ed45
yeji <oyatpeach@naver.com>
2023-03-22 15:49:45 +0900

    feat: create Done component

--- src/components/Done.js
+++ src/components/Done.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Done() {
-  return <div></div>;
+  return (
+    <DoneList>
+      <div id="done-title">{Todo.title}</div>
+      <button id="check-button">✔</button>
+      <button id="delete-button">✖</button>
+    </DoneList>
+  );
 }
+
+const DoneList = styled.div`
+  display: flex;
+  #done-title {
+    text-decoration: line-through;
+  }
+  #delete-button {
+    border: none;
+    background: none;
+    color: rgb(104, 87, 134);
+    font-size: 18px;
+  }
+  #check-button {
+    border: none;
+    background: none;
+    color: rgb(104, 87, 134);
+    font-size: 18px;
+  }
+`;
 
 export default Done;
